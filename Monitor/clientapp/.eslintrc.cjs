0fc960993f1bba6bb7ac938619c26eb0c5e5b2e0
Zoe Lavoie <zoelavoie19@outlook.com>
2023-02-06 10:14:21 -0500

    Fix menu and side bar
    Template for UI
    
    Add Primevue

--- Monitor/clientapp/.eslintrc.cjs
+++ Monitor/clientapp/.eslintrc.cjs
@@ -1,15 +1,20 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution');
 
 module.exports = {
-  root: true,
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier",
-  ],
-  parserOptions: {
-    ecmaVersion: "latest",
-  },
+    root: true,
+    extends: ['plugin:vue/vue3-essential', 'eslint:recommended', '@vue/eslint-config-prettier'],
+    parserOptions: {
+        ecmaVersion: 'latest'
+    },
+    rules: {
+        'vue/multi-word-component-names': 'off',
+        'vue/no-reserved-component-names': 'off',
+        'vue/component-tags-order': [
+            'error',
+            {
+                order: ['script', 'template', 'style']
+            }
+        ]
+    }
 };
